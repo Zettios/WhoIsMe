5ebab13e128ff3c15f0ef48ca455ce3b5bb505fd
Zettios <enricohuig2@gmail.com>
2024-09-16 14:21:38 +0200

    Initital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'WhoIsMe';
+  contentText = 'This is my personal page!';
 }
