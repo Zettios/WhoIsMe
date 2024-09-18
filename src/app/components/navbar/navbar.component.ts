60dca8438d198a34e8d42d0a7d276a907154ab99
Zettios <enricohuig2@gmail.com>
2024-09-18 18:09:23 +0200

    Changed implementation for navbar. Now mobile and desktop compatible.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener  } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
@@ -8,20 +8,15 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   imports: [ NgOptimizedImage ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
-  animations: [
-    trigger('openClose', [
-      state('open', style({left: '0'})),
-      state('closed', style({left: '-100%'})),
-      transition('open => closed', [animate('0.2s')]),
-      transition('closed => open', [animate('0.2s')])
-    ])
-  ]
+  // animations: [
+  //   trigger('openClose', [
+  //     state('open', style({left: '0'})),
+  //     state('closed', style({left: '-100%'})),
+  //     transition('open => closed', [animate('0.2s')]),
+  //     transition('closed => open', [animate('0.2s')])
+  //   ])
+  // ]
 })
 
-export class NavbarComponent {
-  isShowingMobileNavContainer: boolean = false;
-
-  showMobileNavItems() {
-    this.isShowingMobileNavContainer = !this.isShowingMobileNavContainer;
-  }
+export class NavbarComponent { 
 }
